0b808bf47d39840227079174d7a7fdff1ede00f9
Venkata Ajay Mamidipaka <98172062+VenkataAjay31@users.noreply.github.com>
2022-10-30 23:03:54 +0530

    commit

--- savvy/src/App.js
+++ savvy/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import LoginPage from './Pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <LoginPage />
     </>
   );
 }
