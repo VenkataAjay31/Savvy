249fef54df64d34f0741308477e82181c91a19f1
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-10-30 21:26:41 +0530

    SignInWith Component Done

--- savvy/src/App.js
+++ savvy/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import QuickSigIn from './Components/QuickSigIn/QuickSigIn';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <QuickSigIn/>
     </>
   );
 }
---
+++ savvy/src/Components/QuickSigIn/QuickSigIn.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './QuickSigIn.css'
+import fb from '../../Assets/Icons/facebook_ic.svg' 
+import google from '../../Assets/Icons/google_ic.svg' 
+import apple from '../../Assets/Icons/cib_apple.svg' 
+
+export default function QuickSigIn() {
+    return (
+        <>
+            <div className="QuickSign-container ">
+                <div className="loginWithText ">
+                    <hr id='line' size="2" width="33%" color='#585858' ></hr>
+                    <span> Or Login With </span>
+                    <hr id='line' size="2" width="33%" color='#585858' ></hr>
+                </div>
+                <div className="QuickSign-logos">
+                   <div > <img src={fb} alt="" /> </div>
+                   <div > <img src={google} alt="" /> </div>
+                   <div > <img src={apple} alt="" /> </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
