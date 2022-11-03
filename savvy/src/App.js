4506e35752232b87b3741a7f8e16f665d5eba452
Venkata Ajay Mamidipaka <98172062+VenkataAjay31@users.noreply.github.com>
2022-11-03 13:02:42 +0530

    done

--- savvy/src/App.js
+++ savvy/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import QuickSigIn from './Components/QuickSigIn/QuickSigIn';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
 
 function App() {
   return (
-    <LoginPage />
+    <RegistrationPage />
   );
 }
 
--- savvy/src/Pages/RegistrationPage/RegistrationPage.jsx
+++ savvy/src/Pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import './RegistrationPage.css'
+import Illu from '../../Assets/register.svg'
+import QuickSigIn from '../../Components/QuickSigIn/QuickSigIn'
 
 export default function RegistrationPage() {
   return (
-    <div>RegistrationPage</div>
+    <div className='form-img'>
+        <div className='form-comp'>
+        <form>
+            <p className='login'>Register</p>
+            <div className='form-comp1'>
+
+            <div className="form-outline mb-4">
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." className="form-control" />
+            </div>
+
+            <div className="form-outline mb-4">
+            <input type="email" placeholder="Enter email" id="form2Example1" className="form-control" />
+            </div>
+
+            <div className="form-outline mb-4">
+            <input type="password" placeholder="Enter Password" id="form2Example2" className="form-control" />
+            </div>
+
+            <div className='form-outline mb-4'>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" className="form-control" id="psw-repeat" required />
+            </div>
+
+            <div className="row fg-pw mb-4">
+                <div className="col">
+                <a href="#!">Forgot password?</a>
+                </div>
+            </div>
+            </div>
+            
+            <div className='terms'>
+                <label><input type="checkbox" checked="checked" name="sameadr"/>Agree to <span>terms and conditions</span> and <span>privacy policy.</span></label>
+            </div>
+            <button type="button" className="btn btn-primary btn-block mb-4">REGISTER</button>
+            <div className="text-center">
+                <p>Don't have an account? <a href="#!">Register Now</a></p>
+            </div>
+        </form>
+        </div>
+
+        <div className='lg-img'>
+            <img src={Illu} alt="Login-img" />
+        </div>
+    </div>
+
   )
 }
+
