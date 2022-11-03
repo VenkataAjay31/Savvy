0ab77767efdb5c27729d523066e82bbc1ec44fec
Venkata Ajay Mamidipaka <98172062+VenkataAjay31@users.noreply.github.com>
2022-11-03 13:14:28 +0530

    done

--- savvy/src/App.js
+++ savvy/src/App.js
@@ -3,10 +3,11 @@ import QuickSigIn from './Components/QuickSigIn/QuickSigIn';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
+import OTP from './Pages/OTP/OTP';
 
 function App() {
   return (
-    <RegistrationPage />
+    <OTP />
   );
 }
 
