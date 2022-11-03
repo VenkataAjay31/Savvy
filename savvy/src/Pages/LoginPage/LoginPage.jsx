9ef09017bf6af9c2d6df7aba437d6d339c7ec7f8
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-03 19:35:02 +0530

    Login Page is Done

--- savvy/src/Pages/LoginPage/LoginPage.jsx
+++ savvy/src/Pages/LoginPage/LoginPage.jsx
@@ -5,40 +5,44 @@ import QuickSigIn from '../../Components/QuickSigIn/QuickSigIn'
 
 
 export default function LoginPage() {
-  return (
-    <div className='form-img'>
-        <div className='form-comp'>
-        <form>
-            <p className='login'>Log In</p>
-            <div className='form-comp1'>
-            <div className="form-outline mb-4">
-            <input type="email" placeholder="Enter email" id="form2Example1" className="form-control" />
+    return (
+        <div className='form-img '>
+            <div className='form-comp '>
+                <form>
+                    <p className='login'>Login</p>
+                    <div className='form-comp1'>
+                        <div className="form-outline mb-4">
+                            <input type="email" placeholder="Enter email/mobile no." id="form2Example1" className="form-control" />
+                        </div>
+
+                        <div className="form-outline mb-4">
+                            <input type="password" placeholder="Enter Password" id="form2Example2" className="form-control" />
+                        </div>
+
+                        <div className="row fg-pw mb-4">
+                            <div className="col forget-pass">
+                                <a href="#!">Forgot password?</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="button" className="btn btn-primary btn-block mb-4">LOGIN</button>
+                    <div className="text-center">
+                        <p>Don't have an account? <a id='registerlink' href="#!">Register Now</a></p>
+                    </div>
+                </form>
+                    <QuickSigIn/>
+
             </div>
 
-            <div className="form-outline mb-4">
-            <input type="password" placeholder="Enter Password" id="form2Example2" className="form-control" />
+            <div className='lg-img'>
+                <img src={Illu} alt="Login-img" />
             </div>
 
-            <div className="row fg-pw mb-4">
-                <div className="col">
-                <a href="#!">Forgot password?</a>
-                </div>
-            </div>
-            </div>
 
-            <button type="button" className="btn btn-primary btn-block mb-4">LOGIN</button>
-            <div className="text-center">
-                <p>Don't have an account? <a href="#!">Register Now</a></p>
-            </div>
-        </form>
+
         </div>
 
-        <div className='lg-img'>
-            <img src={Illu} alt="Login-img" />
-        </div>
-
-    </div>
-
-  )
+    )
 }
 
