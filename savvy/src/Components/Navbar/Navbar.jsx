392b41f75d90070d0ff7908be1ed75c79a2c7222
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-06 01:24:15 +0530

    Hero-section responsiveness

--- savvy/src/Components/Navbar/Navbar.jsx
+++ savvy/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
 import './Navbar.css'
+import menu from '../../Assets/Icons/menu.svg'
+import cross from '../../Assets/Icons/x.svg'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 
 export default function Navbar() {
+
+
+  function openSidebar() {
+    const element = document.getElementById('sidebar')
+    element.style.transform = "translateX(0%)"
+  }
+
+  function closeSidebar() {
+    const element = document.getElementById('sidebar')
+    element.style.transform = "translateX(100%)"
+  }
+
+
   return (
     <div className='navbar  ' >
       <span className="logo ">Savvy</span>
-      <div className="nav-items "> 
+      <div className="nav-items ">
 
-      <Link  > <span>Home</span> </Link>
-      <Link  > <span>Services</span> </Link>
-      <Link  > <span>About Us</span> </Link>
-      <Link  > <span>Contact Us</span> </Link>
-      <Link to="/login" > <span>Login</span> </Link>
+        <Link  > <span>Home</span> </Link>
+        <Link  > <span>Services</span> </Link>
+        <Link  > <span>About Us</span> </Link>
+        <Link  > <span>Contact Us</span> </Link>
+        <Link to="/login" > <span>Login</span> </Link>
 
       </div>
+
+      <div className="drawer">
+        <img src={menu} alt="" onClick={() => { openSidebar() }} />
+        <div className="sidebar" id='sidebar' >
+          <div className="cancel"><img onClick={() => { closeSidebar() }} src={cross} alt="" /></div>
+          <Link  > <span>Home</span> </Link>
+          <Link  > <span>Services</span> </Link>
+          <Link  > <span>About Us</span> </Link>
+          <Link  > <span>Contact Us</span> </Link>
+          <Link to="/login" > <span>Login</span> </Link>
+        </div>
+      </div>
+
     </div>
   )
 }
