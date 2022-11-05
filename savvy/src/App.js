200c9f26855bd9e0da242cd5f25ee7942aad083c
Venkata Ajay Mamidipaka <98172062+VenkataAjay31@users.noreply.github.com>
2022-11-05 11:28:38 +0530

    update

--- savvy/src/App.js
+++ savvy/src/App.js
@@ -5,10 +5,11 @@ import LoginPage from './Pages/LoginPage/LoginPage';
 import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
 import Password from './Pages/Password/Password';
 import OTP from './Pages/OTP/OTP';
+import Register from './Pages/RegistrationPage/Register';
 
 function App() {
   return (
-    <OTP />
+    <Register />
   );
 }
 
